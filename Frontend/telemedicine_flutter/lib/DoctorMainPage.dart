1c1b665a2f94dd09614dc12cd0106906b7637a41
Nathan12313 <41312297+nathan-boc@users.noreply.github.com>
2022-09-20 22:04:37 +1000

    Added skeleton for doctors' main page

---
+++ Frontend/telemedicine_flutter/lib/DoctorMainPage.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'HelperFunctions.dart';
+
+class DoctorMainPage extends StatelessWidget {
+  const DoctorMainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("MD Telemedicine")),
+      body: ListView(children: [ 
+
+        // My Profile button
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: SizedBox(
+            width: screenWidth(context),
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red[800],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0)),
+                textStyle: const TextStyle(
+                  fontSize: 16, fontFamily: 'Georgia', 
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  // TODO link this up to register account page
+                  context, MaterialPageRoute(builder: (context) => DoctorMainPage()));
+              },
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text("My Profile")
+              )
+            ),
+          ),
+        ),
+
+        // My Patients button
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: SizedBox(
+            width: screenWidth(context),
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red[800],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0)),
+                textStyle: const TextStyle(
+                  fontSize: 16, fontFamily: 'Georgia', 
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  // TODO link this up to register account page
+                  context, MaterialPageRoute(builder: (context) => DoctorMainPage()));
+              },
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text("My Patients")
+              )
+            ),
+          ),
+        ),
+
+        // My Appoinments button
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: SizedBox(
+            width: screenWidth(context),
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red[800],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0)),
+                textStyle: const TextStyle(
+                  fontSize: 16, fontFamily: 'Georgia', 
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  // TODO link this up to register account page
+                  context, MaterialPageRoute(builder: (context) => DoctorMainPage()));
+              },
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text("My Appointments")
+              )
+            ),
+          ),
+        ),
+
+        // My Availabilities button
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: SizedBox(
+            width: screenWidth(context),
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red[800],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0)),
+                textStyle: const TextStyle(
+                  fontSize: 16, fontFamily: 'Georgia', 
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  // TODO link this up to register account page
+                  context, MaterialPageRoute(builder: (context) => DoctorMainPage()));
+              },
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text("My Availabilities")
+              )
+            ),
+          ),
+        ),
+
+        // Chat Menu button
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: SizedBox(
+            width: screenWidth(context),
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red[800],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0)),
+                textStyle: const TextStyle(
+                  fontSize: 16, fontFamily: 'Georgia', 
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  // TODO link this up to register account page
+                  context, MaterialPageRoute(builder: (context) => DoctorMainPage()));
+              },
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Chat Online")
+              )
+            ),
+          ),
+        ),
+        
+      ])
+    );
+  }
+}
