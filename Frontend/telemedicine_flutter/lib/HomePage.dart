c91a5fdafd704ed3e18acad5de0e5f944b3ae128
Nathan12313 <41312297+nathan-boc@users.noreply.github.com>
2022-09-20 18:25:02 +1000

    Fixed home page button titles

--- Frontend/telemedicine_flutter/lib/HomePage.dart
+++ Frontend/telemedicine_flutter/lib/HomePage.dart
@@ -9,52 +9,53 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: const Text('ND Telemedicine')),
-      body: Center( 
-        // TODO : add child for brand image here
+      appBar: AppBar(centerTitle: true, title: const Text('MD Online')),
+      body: ListView(children: [ 
+        Center( 
 
-        // Column for the navigation buttons
-        child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
-            child: SizedBox(
-              width: screenWidth(context) * 0.4,
-              height: 50,
-              child: ElevatedButton(
-                child: const Text('Admin'),
-                onPressed: () => print('Admin button pressed')
+          // Navigation buttons
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+              child: SizedBox(
+                width: screenWidth(context) * 0.4,
+                height: 50,
+                child: ElevatedButton(
+                  child: const Text('Admin Login'),
+                  onPressed: () => print('Admin button pressed')
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
-            child: SizedBox(
-              width: screenWidth(context) * 0.4,
-              height: 50,
-              child: ElevatedButton(
-                child: const Text('Patient Login'),
-                onPressed: () {
-                  Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => PatientLogin()));
-                },
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+              child: SizedBox(
+                width: screenWidth(context) * 0.4,
+                height: 50,
+                child: ElevatedButton(
+                  child: const Text('Patient Login'),
+                  onPressed: () {
+                    Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => PatientLogin()));
+                  },
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
-            child: SizedBox(
-              width: screenWidth(context) * 0.4,
-              height: 50,
-              child: ElevatedButton(
-                child: const Text('Patient'),
-                onPressed: () {
-                  Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => Profile_screen()));
-                }
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 60, 0, 60),
+              child: SizedBox(
+                width: screenWidth(context) * 0.4,
+                height: 50,
+                child: ElevatedButton(
+                  child: const Text('Doctor Login'),
+                  onPressed: () {
+                    Navigator.push(
+                      context, MaterialPageRoute(builder: (context) => Profile_screen()));
+                  }
+                ),
               ),
-            ),
-          )
-        ]),
+            )
+          ]),
+        ),]
       ),
     );
   }
