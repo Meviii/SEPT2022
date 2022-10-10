ba6c3461b700c89e89405fa986b36456d5d72e5e
Meviii <misaluk20@gmail.com>
2022-10-11 09:13:20 +1100

    fixed user list for users and added adminlist

--- Backend/AccountService/src/main/java/com/mdonline/AccountService/Model/AdminList.java
+++ Backend/AccountService/src/main/java/com/mdonline/AccountService/Model/AdminList.java
@@ -1,5 +1,32 @@
 package com.mdonline.AccountService.Model;
 
-//TODO
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdminList {
+
+    private List<Admin> admins;
+
+    public AdminList(){
+        admins = new ArrayList<>();
+    }
+
+    public AdminList(List<Admin> admins) {
+        this.admins = admins;
+    }
+
+    public List<Admin> getAdmins() {
+        return admins;
+    }
+
+    public void setAdmins(List<Admin> admins) {
+        this.admins = admins;
+    }
+
+    @JsonIgnore
+    public Boolean isEmpty(){
+        return admins.isEmpty();
+    }
 }
